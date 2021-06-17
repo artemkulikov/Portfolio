0ec0145371525f8dc9f6f4183da00a1f6191fd3a
Artem Kulikov <78357375+artemkulikov@users.noreply.github.com>
2021-06-17 09:26:07 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
-$(".header-btn").on("click", function (e) {
-    $(this).toggleClass("on"), e.preventDefault(), $(".header-nav").toggleClass("active"), $(".header-nav").toggleClass("block"), $("body").toggleClass("locked"), $(".header-desc ul").toggleClass("blockUL")
+$('.header-btn').on('click', function (e) {
+    $(this).toggleClass('on'), e.preventDefault(), $('.header-nav').toggleClass('active'), $('.header-nav').toggleClass('block'), $('body').toggleClass('locked'), $('.header-desc ul').toggleClass('blockUL')
 });
-
-$(document).ready(function() {
-	$('.go_to').click(function() {
-		$('.header-nav').removeClass('active'), $('.header-btn').removeClass("on"), $('.header-nav').removeClass('block'), $("body").removeClass("locked"), $(".header-desc ul").removeClass("blockUL");
-	});
-});
-
 $(document).ready(function () {
-    $(".go_to").click(function (t) {
+    $('.go_to').click(function (t) {
         t.preventDefault();
-        const o = $(this).attr("href");
-        0 != $(o).length && $("html, body").animate({
+        const o = $(this).attr('href');
+        0 != $(o).length && $('html, body').animate({
             scrollTop: $(o).offset().top
         }, 1e3)
     })
 });
+
+$(document).ready(function() {
+	$('.go_to').click(function() {
+		$('.header-nav').removeClass('active'), $('.header-btn').removeClass('on'), $('.header-nav').removeClass('block'), $('body').removeClass('locked'), $('.header-desc ul').removeClass('blockUL');
+	});
+});
+
+
 $(document).ready(function () {
     $(window).on("scroll", function () {
-        $(window).scrollTop() ? ($(".header-nav").addClass("whiteBG"), $(".header-nav").removeClass("container")) : ($(".header-nav").removeClass("whiteBG"), $(".header-nav").addClass("container"))
+        $(window).scrollTop() ? ($('.header-nav').addClass('whiteBG'), $('.header-nav').removeClass('container')) : ($('.header-nav').removeClass('whiteBG'), $('.header-nav').addClass("container"))
     })
 });
